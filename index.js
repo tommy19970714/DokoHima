5b8e67992ab4d738581738d425c4e81858fa3c16
tommy19970714 <toshiki0714@icloud.com>
2017-07-19 18:39:12 +0900

    add botkit

--- index.js
+++ index.js
@@ -17,4 +17,34 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: "xoxb-214617764931-HUDSOTjYAnarrRM6O5tADU0o"
+}).startRTM(function(err, bot, payload){
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+});
 
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
