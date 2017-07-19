23c117b3d5fa6395527bf6cd69e8f2f9402b8877
tommy19970714 <toshiki0714@icloud.com>
2017-07-19 22:20:58 +0900

    miss

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ controller.on('direct_message,direct_mention,mention', function(bot, message) {
     var mode = message.text.substr(message.text.indexOf(7)+4,1);
     if(mode == 'd') {
         var detail = message.text.substr(message.text.indexOf(7)+5)
-        query('UPDATE building7 name = $2~ WHERE detail = $3~', [room, detail]);
+        query('UPDATE building7 name = $1~ WHERE detail = $2~', [room, detail]);
         query('INSERT INTO building7 VALUES($1~, $2~, $3~,now())', [room, "none", detail]);
     } else {
         var status = message.text.substr(message.text.indexOf(7)+4);
-        query('UPDATE building7 name = $2~ WHERE status = $3~', [room, status]);
+        query('UPDATE building7 name = $1~ WHERE status = $2~', [room, status]);
         query('INSERT INTO building7 VALUES($1~, $2~, $3~,now())', [room, status, "none"]);
     }
 });
