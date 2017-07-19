16a165791f2aa7f8f86f75c5ec92597ee2aa7424
tommy19970714 <toshiki0714@icloud.com>
2017-07-19 19:24:59 +0900

    change bot reply

--- index.js
+++ index.js
@@ -20,31 +20,12 @@ app.listen(app.get('port'), function() {
 var Botkit = require('botkit');
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-  token: "xoxb-214617764931-HUDSOTjYAnarrRM6O5tADU0o"
-}).startRTM(function(err, bot, payload){
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
-});
+    token: "xb-214617764931-HUDSOTjYAnarrRM6O5tADU0o"
+}).startRTM();
 
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
     controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
+        bot.reply(message, 'OK!!');
     });
 });
