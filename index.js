4208726a2792cb5197b9b0743a2165ecf72dff11
Senri Yoshikawa <s13204@tokyo.kosen-ac.jp>
2017-07-21 02:40:28 +0900

    I'm sleepy

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views/pages');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  response.render('index', {title : 'タイトル',
+  s7308 : '眠い1',
+  s7408 : '眠い2',
+  s7501 : '眠い3',
+  s7505 : '眠い4',
+  s7506 : '眠い5',
+  d7308 : '眠い6',
+  d7408 : '眠い7',
+  d7501 : '眠い8',
+  d7505 : '眠い9',
+  d7506 : '眠い10'}
+);
   response.render('pages/index');
 });
 
@@ -39,8 +51,20 @@ controller.on('direct_message,direct_mention,mention', function(bot, message) {
     if(mode.includes("d")) {
         var detail = message.text.substr(message.text.indexOf(7)+5)
         db.none("INSERT INTO building7 VALUES ('" + room + "','none','"+ detail +"', now()) ON CONFLICT ON CONSTRAINT building7_pkey DO UPDATE SET detail='" + detail +"'");
+        db.one("SELECT * FROM building7 WHERE room='7308'").then(data => {
+              console.log("---------------------------------------------")
+              console.log(data);
+              console.log("---------------------------------------------")
+              data_array.push(data);
+          });
     } else {
         var status = message.text.substr(message.text.indexOf(7)+4);
         db.none("INSERT INTO building7 VALUES ('" + room + "','"+ status +"','none', now()) ON CONFLICT ON CONSTRAINT building7_pkey DO UPDATE SET status='" + status +"'");
+        db.one("SELECT * FROM building7 WHERE room='7308'").then(data => {
+              console.log("---------------------------------------------")
+              console.log(data);
+              console.log("---------------------------------------------")
+          });
     }
+
 });
